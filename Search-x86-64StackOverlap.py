6d6ac49abd18f638cea90ca6c1c5b7d13ab02518
PYozo <50827329+PYozo@users.noreply.github.com>
2021-08-20 01:24:51 +0800

    Add files via upload

--- Search-x86-64StackOverlap.py
+++ Search-x86-64StackOverlap.py
@@ -78,14 +78,56 @@ def getArgAddr(func_addr,argNum):
 #
 def getArgs(addr):
     x86mov=['mov','lea']
+    args=[]
     #如果是参数不是寄存器传值(寄存器传值的返回值为1)
-    if get_operand_type(addr,1) !=1:
+    if idc.get_operand_type(addr,1) !=1:
         arg=idc.print_operand(addr,1)
-        set_cmt()
-        
+        if "offset" in arg:
+            return arg[7:]
+        else:
+            return arg
+    #如果是寄存器的话,需要递归处理找到最后一个不是寄存器的参数
+    func_start=idaapi.get_func(addr).start_ea
+    arg_addr=addr
+    target=idc.print_operand(addr,1)
+    while arg_addr<=start_ea:
+        arg_addr=idc.get_first_cref_to(arg_addr)
+        if idc.idc.print_insn_mnem(arg_addr) in x86mov and idc.print_operand(arg_addr,0)==target:
+            return idc.print_operand(arg_addr,1)
+    pass
+def auditFormat(func_addr,func_name,arg_num):
+    pass
+    
+    
+def auditAddr(func_addr,func_name,arg_num):
+    #local buf size
+    local_buf_size=idc.get_func_attr(func_addr,FUNCATTR_FRSIZE)
+    if local_buf_size==BADADDR:
+        local_buf_size="get fail"
+    else:
+        local_buf_size="0x%x"%local_buf_size
+    return 
+    #get arg
+    pass
+    
+       
 def audit(func_name):
     func_addr=getFuncAddr(func_name)
-    while 
+    if func_name in one_arg_function:
+        arg_num = 1
+    elif func_name in two_arg_function:
+        arg_num = 2
+    elif func_name in three_arg_function:
+        arg_num = 3
+    elif func_name in format_function_offset_dict:
+        arg_num = format_function_offset_dict[func_name] + 1
+    else:
+        print("The %s function didn't write in the describe arg num of function array,please add it to,such as add to `two_arg_function` arary" % func_name)
+        return
+    if func_name in format_function_offset_dict:
+        auditFormat(func_addr,func_name,arg_num)
+    else:
+        auditAddr(func_addr,func_name,argnum)
         
 def main_Audit():
     print('Auditing dangerous functions ......')
